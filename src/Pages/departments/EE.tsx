a21a0bd935efb64dbc0597de318421afb6e0367c
Glorie May Verayo <gloriemayverayo06@gmail.com>
2026-03-27 11:32:19 +0800

    Changes Again Capslock

--- src/Pages/departments/EE.tsx
+++ src/Pages/departments/EE.tsx
@@ -58,7 +58,7 @@ export default function EEPage() {
                 College of Engineering
               </p>
               
-              <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-black italic tracking-tighter leading-[0.9] lg:leading-[0.85] lowercase">
+              <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-black italic tracking-tighter leading-[0.9] lg:leading-[0.85] uppercase">
                 <span className="block text-black">Electrical</span>
                 <span className="inline-block bg-linear-to-r from-red-700 to-orange-500 bg-clip-text text-transparent lg:pr-8 lg:-mr-8">
                   Engineering
