5a29915b25c7f872d30ca9d34f7770378b33bf3e
robertjustinchavez <robertjustin.chavez@bulsu.edu.ph>
2026-03-29 01:58:51 +0800

    Update navbar.tsx
    test working

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar({
       { id: "so" as const, label: "SO" },
       { id: "curriculum" as const, label: "Curriculum" },
       { id: "laboratories" as const, label: "Laboratories" },
-      { id: "faculty" as const, label: "Faculty" },
+      { id: "faculty" as const, label: "FA CUTE" },
       { id: "careers" as const, label: "Careers" },
     ],
     []
