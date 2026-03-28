55f9d54f0019107adcb9175076f0dcc444722511
Glorie May Verayo <146597200+gmeiv@users.noreply.github.com>
2026-03-28 20:27:27 +0800

    department select change

--- src/Pages/DepartmentSelect.tsx
+++ src/Pages/DepartmentSelect.tsx
@@ -16,7 +16,7 @@ export default function DepartmentSelect() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold mb-8">
-        BulSU College of Engineering
+        BulSU College of EngineeringGGGGG
       </h1>
 
       <p className="text-gray-600 mb-10">
